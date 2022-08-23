556cead3b637861b5e4808db8d8fc5e08aac67d2
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-08-23 16:23:03 -0300

    Realizado a refatoração das funções

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 console.log("Start JS");
 
+const btnTry = document.querySelector(".screen1 form > button");
+const btnReset = document.querySelector(".screen2 form > button");
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+
 const randomNumber = Math.round(Math.random() * 10);
 console.log(randomNumber)
 
 let xAttempts = 1;
-console.log(xAttempts);
 
+btnTry.addEventListener("click", handleTryClick);
 
-const btnTry = document.querySelector(".screen1 form > button");
-const btnReset = document.querySelector(".screen2 form > button");
-const screen1 = document.querySelector(".screen1");
-const screen2 = document.querySelector(".screen2")
+btnReset.addEventListener("click", handleResetClick);
 
 function handleTryClick(event) {
     event.preventDefault();
@@ -18,23 +20,20 @@ function handleTryClick(event) {
     const inputNumber = document.querySelector("#inputNumber");
 
     if(Number(inputNumber.value) == randomNumber) {
-        console.log(inputNumber.value);
-        screen1.classList.add("hide");
-        screen2.classList.remove("hide");
-
+        toggleScreen();
         document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativas`
     }
 
     inputNumber.value = "";
     xAttempts++;
-
 }
 
+function toggleScreen(){
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
 
-btnTry.addEventListener("click", handleTryClick);
-
-btnReset.addEventListener("click",function() {
-    screen1.classList.remove("hide");
-    screen1.classList.add("hide");
+function handleResetClick() {
+    toggleScreen();
     xAttempts = 1;
-})
+}
